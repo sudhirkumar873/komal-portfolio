7c6384245cc308f5139d82dcd2ed0ae0c1bc6691
Sudhir Kumar <80701339+sudhirkumar873@users.noreply.github.com>
2024-10-04 16:28:59 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:komalavutapalli06@gmail.com">komalavutapalli06@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a
+            href="www.linkedin.com/in/komal-naga-teja-avutapalli-a42aa8165"
+          >
+            linkedin.com/KomalAvutapalli
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Komal-Avutapalli">github.com/KomalAvutapalli</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
